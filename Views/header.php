76834ff04c75ea815dabaca1959a5d54406428c6
Hycarus <mirkomarasco08@gmail.com>
2023-12-05 15:16:14 +0100

    refactoring header

--- Views/header.php
+++ Views/header.php
@@ -31,7 +31,7 @@ session_start();
         </div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand w-25" href="#">
+                <a class="navbar-brand w-25" href="index.php">
                     <img class="w-25" src="img/logo.png" alt="OOP - Shop">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -51,20 +51,6 @@ session_start();
                         <li class="nav-item">
                             <a class="nav-link" href="games.php">Games</a>
                         </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-
                     </ul>
                     <form class="d-flex mx-2" role="search">
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
