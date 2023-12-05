9523d87d1c6fd2eabaecdcf8d4cec6398174383e
Hycarus <mirkomarasco08@gmail.com>
2023-12-05 10:17:26 +0100

    changed card style

--- Views/card.php
+++ Views/card.php
@@ -1,23 +1,25 @@
 <div class="col-12 col-md-4 col-lg-3">
-    <div class="card">
-        <img src="<?= $image ?>" class="card-img-top my-ratio" alt="<?= $title ?>">
-        <div class="card-body">
+    <div class="card my-3 ">
+        <img src="<?= $image ?>" class="card-img-top my-img-card-top" alt="<?= $title ?>">
+        <div class="card-body overflow-y-auto my-card-body">
             <h5 class="card-title">
                 <?= $title ?>
             </h5>
-            <p class="card-text">
+            <p class="card-text my-card-content">
                 <?= $content ?>
             </p>
             <div class="d-flex justify-content-between align-items-flex-start">
                 <?= $custom ?>
-
             </div>
-            <?php foreach ($genre as $item) { ?>
-                <div>
-                    <?= $item->name ?>
-                </div>
-            <?php } ?>
-            <div class="w-25">
+            <div>
+                Generi:
+                <?php foreach ($genre as $item) { ?>
+                    <div>
+                        <?= $item->name ?>
+                    </div>
+                <?php } ?>
+            </div>
+            <div class="my-flag">
                 <img class="w-100" src="<?= $flag ?>" alt="">
             </div>
         </div>
